6412a197eeba8a490196e1d22178f5ff217a9fdb
Evence <evencewu@outlook.com>
2022-12-14 19:50:54 +0800

    1

--- 3.document/论文资料/仿真/Moco仿真/QuadrupedSim-master/forward_kinematics.m
+++
@@ -1,8 +0,0 @@
-function end_effector = forward_kinematics(s, u, k, body)
-    % from center of the shoulder, 
-    l1 = body.upper_length;
-    l2 = body.lower_length;
-    end_effector = [ - l2*sin(k + u)       -  l1*sin(u);
-                     sin(s)*(l2*cos(k + u) +  l1*cos(u));
-                    -cos(s)*(l2*cos(k + u) +  l1*cos(u))   ];
-end
